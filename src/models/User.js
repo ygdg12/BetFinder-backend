95272756e919987dd2dd77f30958ce2f14b4f310
Yared Girma <yaredgirmab1234@email.com>
2026-05-03 11:49:05 +0300

    fix 4

--- src/models/User.js
+++ src/models/User.js
@@ -22,14 +22,11 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", async function saveHook(next) {
+userSchema.pre("save", async function hashPassword() {
   if (!this.isModified("password")) {
-    next();
     return;
   }
-
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 userSchema.methods.comparePassword = function comparePassword(plainPassword) {
